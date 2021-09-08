9008900e3fd94dd9d7096f8f8e2045d324c468d3
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2021-09-09 00:34:44 +0200

    Add files via upload
    Work in progress

---
+++ logic.js
@@ -0,0 +1,16 @@
+/* This is not working for some reason. Will fix
+
+const capsWarn = document.getElementById('capsWarning');
+const capsBtn = document.etElementById('capsLock')
+
+capsBtn.addEventListener("click", function() {
+    if (capsWarn.style.display === block) {
+    capsWarn.style.display = "none";
+    }
+    else {
+    capsWarn.style.display = "block"
+    }
+})
+
+*/
+
